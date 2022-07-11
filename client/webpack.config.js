47ac549a7037121253b23f57394b59054ff881aa
happyGyu <happyboyto.dev@gmail.com>
2022-07-12 01:38:22 +0900

    style: :art: index.html 경로 변경

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: path.resolve(__dirname, 'src', 'index.html'),
     }),
   ],
 };
