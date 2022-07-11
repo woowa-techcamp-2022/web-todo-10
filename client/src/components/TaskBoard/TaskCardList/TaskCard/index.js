02934339ea8235d978dc5ecbecd1928919922568
happyGyu <happyboyto.dev@gmail.com>
2022-07-12 00:39:43 +0900

    feat: :sparkles: card component 구현

---
+++ client/src/components/TaskBoard/TaskCardList/TaskCard/index.js
@@ -0,0 +1,38 @@
+import './index.scss';
+
+export default class TaskCard {
+  #$parent;
+  #cardData;
+
+  constructor($parent, cardData) {
+    this.#$parent = $parent;
+    this.#cardData = cardData;
+    this.$taskCard = document.createElement('div');
+  }
+
+  render() {
+    this.$taskCard.className = 'taskCard';
+    this.$taskCard.innerHTML = this.#getInnerTemplate();
+    this.#$parent.append(this.$taskCard);
+  }
+
+  #getInnerTemplate() {
+    const { title, details, author } = this.#cardData;
+    return `
+        <h3 class="taskCard__title">${title}</h3>
+        <ul class="taskCard__detail-container">
+          ${this.#getTaskDetailTemplate(details)}
+        </ul>
+        <span class="taskCard__author">author by ${author}</span>
+        <button class="util__btn util__btn--delete taskCard__delete-btn"></button>
+    `;
+  }
+
+  #getTaskDetailTemplate(details) {
+    return details
+      .map((detail) => {
+        `<li class="taskCard__detail">${detail}</li>`;
+      })
+      .join('');
+  }
+}
