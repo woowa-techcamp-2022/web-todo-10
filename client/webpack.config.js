8bfe1a113019ccf510acce6a162ea19cc6ed6edb
happyGyu <happyboyto.dev@gmail.com>
2022-07-11 18:36:19 +0900

    feat: :tada: 웹팩 초기 설정
    우선, development build config설정. 폴더 구조 설정.

---
+++ client/webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    filename: "[name]_bundel.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  watch: true,
+  devtool: "eval",
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "src") },
+    extensions: [".js", ".scss"],
+  },
+};
