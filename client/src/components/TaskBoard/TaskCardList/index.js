35ea95af59ab8d4cbd6a1e6bf2561ab90f9ff861
happyGyu <happyboyto.dev@gmail.com>
2022-07-12 21:54:40 +0900

    refactor: taskcardlist 함수형으로 변경

--- client/src/components/TaskBoard/TaskCardList/index.js
+++ client/src/components/TaskBoard/TaskCardList/index.js
@@ -1,75 +1,38 @@
 import './index.scss';
-import TaskCard from './TaskCard';
-import EditingTaskCard from './EditingTaskCard';
+import { makeTaskCardElement } from './TaskCard';
 
-export default class TaskCardList {
-  #$taskCardList;
-  #taskCards;
+export const makeTaskCardListElement = (cardListData) => {
+  const { listName, tasks } = cardListData;
+  const $taskCardList = document.createElement('section');
+  $taskCardList.className = 'taskcard-list';
+  $taskCardList.innerHTML = getInnerTemplate(listName, tasks.length);
+  tasks.forEach((taskData) =>
+    $taskCardList.append(makeTaskCardElement(taskData))
+  );
+  activateElement();
+  return $taskCardList;
+};
 
-  constructor($parent, cardListData) {
-    this.$parent = $parent;
-    this.cardListData = cardListData;
-    this.#makeTaskCardListElement();
-    this.#makeTaskCards();
-  }
-
-  #makeTaskCardListElement() {
-    this.#$taskCardList = document.createElement('section');
-    this.#$taskCardList.className = 'taskcard-list';
-  }
-
-  #makeTaskCards() {
-    // 데이터 포맷팅 (나중에 달라질 수 있음)
-    const cardDataArr = this.cardListData.tasks;
-    this.#taskCards = cardDataArr.map(
-      (cardData) => new TaskCard(this.#$taskCardList, cardData)
-    );
-  }
-
-  render() {
-    this.#fillTaskCardListElement();
-    this.$parent.append(this.#$taskCardList);
-  }
-
-  #fillTaskCardListElement() {
-    this.#$taskCardList.innerHTML = this.#getInnerTemplate();
-    this.#renderTaskCards();
-    this.#activate();
-  }
-
-  #getInnerTemplate() {
-    const { listName, tasks } = this.cardListData;
-    return `
+const getInnerTemplate = (listName, tasksCnt) => {
+  return `
       <div class="taskcard-list__header">
       <div class="taskcard-list__info">
         <h2 class="taskcard-list__title">${listName}</h2>
-        <div class="taskcard-list__count">${tasks.length}</div>
+        <div class="taskcard-list__count">${tasksCnt}</div>
       </div>
       <div class="taskcard-list__utils">
         <button class="taskcard-list__add-btn util__btn util__btn--add"></button>
         <button class="taskcard-list__delete-btn util__btn util__btn--delete"></button>
       </div>
     `;
-  }
+};
 
-  #renderTaskCards() {
-    this.#taskCards.forEach((taskCard) => taskCard.render());
-  }
+const activateElement = ($taskCardList) => {
+  const $addBtn = $taskCardList.querySelector('.taskcard-list__add-btn');
+  $addBtn.addEventListener('click', addNewTaskCard.bind(null, $taskCardList));
+};
 
-  #activate() {
-    this.#activateAddBtn();
-  }
-
-  #activateAddBtn() {
-    const $addBtn = this.#$taskCardList.querySelector(
-      '.taskcard-list__add-btn'
-    );
-    $addBtn.addEventListener('click', () => this.#addNewTaskCard());
-  }
-
-  #addNewTaskCard() {
-    const newTaskCard = new EditingTaskCard(this.#$taskCardList);
-    this.#taskCards.unshift(newTaskCard);
-    this.#fillTaskCardListElement();
-  }
-}
+const addNewTaskCard = ($taskCardList) => {
+  const $editingTaskCard = makeEditingTaskCard();
+  $taskCardList.insertAdjacentElement('afterbegin', $editingTaskCard);
+};
