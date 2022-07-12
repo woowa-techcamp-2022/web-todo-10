feaeba5e25f8b4c7d20afdf788c62a2ca4f445d0
happyGyu <happyboyto.dev@gmail.com>
2022-07-13 01:36:15 +0900

    feat: 새 카드 post mock api 구현

--- client/src/mocks/handlers.js
+++ client/src/mocks/handlers.js
@@ -5,9 +5,28 @@ const getAllTaskList = (_, res, ctx) => {
   return res(ctx.status(200), ctx.json(taskListDatas));
 };
 
-const test = (req, res, ctx) => {
-  return;
+const addNewCard = (req, res, ctx) => {
+  const { listId, title, details } = req.body;
+  const newTaskCard = {
+    id: new Date().getTime(),
+    title,
+    details,
+    author: 'web',
+  };
+  const targetList = taskListDatas.find((listData) => listData.id === +listId);
+  if (!targetList) {
+    ctx.status(404),
+      ctx.json({
+        status: 'NOT_FOUND',
+        message: '잘못된 리스트 아이디입니다.',
+      });
+  }
+  targetList.tasks.unshift(newTaskCard);
+  return res(ctx.status(200), ctx.json(targetList));
 };
 
-const handlers = [rest.get('/api/tasklists', getAllTaskList)];
+const handlers = [
+  rest.get('/api/tasklists', getAllTaskList),
+  rest.post('/api/taskcard', addNewCard),
+];
 export default handlers;
