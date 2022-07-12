f05e17d9ad0ea51954fdfa01539ca5b0127781d2
happyGyu <happyboyto.dev@gmail.com>
2022-07-12 17:20:54 +0900

    fix: EditingTaskCard 렌더 오류 수정
    Co-authored-by suhwan2004 <suhwan2004@gmail.com>

--- client/src/components/TaskBoard/TaskCardList/EditingTaskCard/index.js
+++ client/src/components/TaskBoard/TaskCardList/EditingTaskCard/index.js
@@ -24,21 +24,21 @@ export default class EditingTaskCard {
   #getEditingTaskDetailTemplate(details) {
     const originalDetailContent = details ? details.join('\n') : '';
     return `
-        <textarea placeholder="내용을 입력하세요" class="taskCard__detail--editing">
-            ${originalDetailContent}
-        </textarea>
+        <textarea placeholder="내용을 입력하세요" class="taskCard__detail--editing">${originalDetailContent}</textarea>
     `;
   }
 
   #getInnerTemplate() {
     const { title, details } = this.originalCardData;
     return `
-            <input type = "text" class="taskCard__title editing" placeholder="제목을 입력하세요">${title}</h3>
-            ${this.#getEditingTaskDetailTemplate(details)}
-            <div class="util__btns">
-                <button class="util__btn--big util__btn--cancel">취소</button>
-                <button class="util__btn--big util__btn--confirm">등록</button>
-            </div>
-        `;
+      <input type = "text" class="taskCard__title editing" placeholder="제목을 입력하세요" value=${
+        title ? title : ''
+      }>
+      ${this.#getEditingTaskDetailTemplate(details)}
+      <div class="util__btns">
+          <button class="util__btn--big util__btn--cancel">취소</button>
+          <button class="util__btn--big util__btn--confirm">등록</button>
+      </div>
+    `;
   }
 }
